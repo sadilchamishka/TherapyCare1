800da36fd4310a4088eb9fe4344843251355b14c
sadilchamishka <sandilchamishka@gmail.com>
2020-06-30 19:34:58 +0530

    any message goes here

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const path = require('path');
 var app = express();
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
+    app.use(express.static(path.join(__dirname, 'build')));
+
     app.get('/', (req, res) => {
       res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
