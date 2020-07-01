6ec936a4ad41e57bd2e883f7f7a25036943521b0
sadilchamishka <sandilchamishka@gmail.com>
2020-07-01 19:29:12 +0530

    express server

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+const path = require('path');
+
+var app = express();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'build')));
+
+    app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+  }
+
+app.listen(process.env.PORT || 8080);
